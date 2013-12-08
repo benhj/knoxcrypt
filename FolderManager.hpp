f7bc5b0d2f43542be765c0f80c9a36b93a7ac530
Ben Jones <benjones@mactop.local>
2013-12-08 21:07:11 +0000

    FolderManager will allow adding of files and folders to a given folder entry

---
+++ FolderManager.hpp
@@ -0,0 +1,31 @@
+#ifndef BFS_FOLDER_MANAGER_HPP__
+#define BFS_FOLDER_MANAGER_HPP__
+
+#include "FileEntry.hpp"
+#include "FolderEntry.hpp"
+
+namespace bfs
+{
+class FolderManager
+{
+public:
+	FolderManager(std::string const &name);
+
+	void addFile(std::string const& name, std::iostream &fileData);
+	void addFolder(std::string const& name);
+
+	void getFile(std::string &name, std::ostream &output);
+	void getFile(uint64_t const fileId, std::ostream &output);
+
+	void removeFile(std::string const &name);
+	void removeFolder(std::string const &name);
+
+	std::vector<std::string> listContents();
+
+private:
+	FolderEntry m_thisFolderEntry;
+
+};
+}
+
+#endif // BFS_FOLDER_MANAGER_HPP__
