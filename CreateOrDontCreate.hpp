39ba19c43c0778319821d2acf0f87ea26aa9a07f
benhj <bhj.research@gmail.com>
2013-12-18 09:59:40 +0000

    Extending ways to open files (e.g. truncate)

---
+++ CreateOrDontCreate.hpp
@@ -0,0 +1,10 @@
+#ifndef BFS_CREATE_OR_DONTCREATE_HPP__
+#define BFS_CREATE_OR_DONTCREATE_HPP__
+
+namespace bfs
+{
+    enum class CreateOrDontCreate { Create, DontCreate };
+}
+
+
+#endif // BFS_CREATE_OR_DONTCREATE_HPP__
---
+++ TruncateOrKeep.hpp
@@ -0,0 +1,10 @@
+#ifndef BFS_TRUNCATE_OR_KEEP_HPP__
+#define BFS_TRUNCATE_OR_KEEP_HPP__
+
+namespace bfs
+{
+    enum class TruncateOrKeep { Truncate, Keep };
+}
+
+
+#endif // BFS_TRUNCATE_OR_KEEP_HPP__
