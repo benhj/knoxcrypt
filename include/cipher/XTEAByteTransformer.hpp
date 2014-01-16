2d0252f2805a383ae15db064e35cfcf7aca8aa2b
benhj <bhj.research@gmail.com>
2014-01-16 15:17:29 +0000

    Bare bones of xtea implementation finished

--- include/cipher/XTEAByteTransformer.hpp
+++ include/cipher/XTEAByteTransformer.hpp
@@ -52,19 +52,6 @@ namespace bfs { namespace cipher
             v[0]=v0; v[1]=v1;
         }
 
-        // the xtea encipher algorithm as found on wikipedia
-        void decipher(unsigned int num_rounds, uint32_t v[2], uint32_t const key[4])
-        {
-            unsigned int i;
-            uint32_t v0=v[0], v1=v[1], delta=0x9E3779B9, sum=delta*num_rounds;
-            for (i=0; i < num_rounds; i++) {
-                v1 -= ((v0 << 4 ^ v0 >> 5) + v0) ^ (sum + key[(sum>>11) & 3]);
-                sum -= delta;
-                v0 -= ((v1 << 4 ^ v1 >> 5) + v1) ^ (sum + key[sum & 3]);
-            }
-            v[0]=v0; v[1]=v1;
-        }
-
         // helper code found here:
         // http://codereview.stackexchange.com/questions/2050/codereview-tiny-encryption-algorithm-for-arbitrary-sized-data
         void convertBytesAndEncipher(unsigned int num_rounds, unsigned char * buffer, uint32_t const key[4])
@@ -110,6 +97,62 @@ namespace bfs { namespace cipher
                          bool encrypt) const
         {
 
+            // how many blocks required? defaults to 1, if length greater
+            // than 8 bytes then more blocks are needed
+            long blocksRequired = 0;
+            if(length > 8) {
+                long remainder = length % 8;
+                long roundedDown = length - remainder;
+                blocksRequired += (roundedDown / 8);
+
+                long c = 0;
+                for(long i = 0;i < blocksRequired; ++i) {
+                    uint8_t cipherStream[8];
+                    for(int j = 0; j < 8; ++j) {
+                        cipherStream[j] = startPosition + c;
+                        ++c;
+                    }
+                    c -= 8;
+                    // todo: encipher here!
+                    // now xor plain with key stream
+                    for(int j = 0; j < 8; ++j) {
+                        out[c] = in[c] ^ cipherStream[j];
+                        ++c;
+                    }
+                }
+
+                if(remainder > 0) {
+
+                    uint8_t cipherStream[8];
+                    for(int j = 0; j < 8; ++j) {
+                        cipherStream[j] = startPosition + c;
+                        ++c;
+                    }
+
+                    c -= 8;
+
+                    // todo: encipher here!
+
+                    // xor rest of stream
+                    for(int j = 0; j < remainder; ++j) {
+                        out[c] = in[c] ^ cipherStream[j];
+                        ++c;
+                    }
+
+                }
+            } else {
+                uint8_t cipherStream[8];
+                for(int j = 0; j < 8; ++j) {
+                    cipherStream[j] = startPosition + j;
+                }
+
+                // todo: encipher here!
+
+                // xor rest of stream
+                for(int j = 0; j < length; ++j) {
+                    out[j] = in[j] ^ cipherStream[j];
+                }
+            }
         }
 
       private:
