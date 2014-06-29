7041d0d6978d21411a3f7312293bab0c67afa1ac
benhj <bhj.research@gmail.com>
2014-06-29 09:45:06 +0100

    tidying up GUI..

--- gui/gui/MainWindow.cpp
+++ gui/gui/MainWindow.cpp
@@ -122,25 +122,39 @@ MainWindow::~MainWindow()
 
 void MainWindow::loadFileButtonHandler()
 {
-    teasafe::SharedCoreIO io(boost::make_shared<teasafe::CoreTeaSafeIO>());
-    io->path = QFileDialog::getOpenFileName().toStdString();
-    io->password = QInputDialog::getText(this, tr("Password dialog"),
-                                         tr("Password:"), QLineEdit::NoEcho).toStdString();
-    io->rootBlock = 0;
+    QFileDialog dlg( NULL, tr("Open container"));
+    dlg.setAcceptMode( QFileDialog::AcceptOpen );
+    dlg.setFileMode(QFileDialog::ExistingFile);
 
-    // give the cipher generation process a gui callback
-    long const amount = teasafe::detail::CIPHER_BUFFER_SIZE / 100000;
-    boost::function<void(teasafe::EventType)> f(boost::bind(&MainWindow::cipherCallback, this, _1, amount));
-    io->ccb = f;
+    if (dlg.exec()) {
 
-    // create a progress dialog to display progress of cipher generation
-    m_sd = boost::make_shared<QProgressDialog>("Generating key...", "Cancel", 0, 0, this);
-    m_sd->setWindowModality(Qt::WindowModal);
+        ui->fileTree->clear();
 
-    // start loading of TeaSafe image
-    m_loaderThread.setSharedIO(io);
-    m_loaderThread.start();
-    m_sd->exec();
+        teasafe::SharedCoreIO io(boost::make_shared<teasafe::CoreTeaSafeIO>());
+        io->path = dlg.selectedFiles().at(0).toStdString();
+
+        bool ok;
+        io->password = QInputDialog::getText(this, tr("Password dialog"),
+                                             tr("Password:"), QLineEdit::NoEcho, "", &ok).toStdString();
+
+        if((!io->password.empty() && ok)) {
+            io->rootBlock = 0;
+
+            // give the cipher generation process a gui callback
+            long const amount = teasafe::detail::CIPHER_BUFFER_SIZE / 100000;
+            boost::function<void(teasafe::EventType)> f(boost::bind(&MainWindow::cipherCallback, this, _1, amount));
+            io->ccb = f;
+
+            // create a progress dialog to display progress of cipher generation
+            m_sd = boost::make_shared<QProgressDialog>("Generating key...", "Cancel", 0, 0, this);
+            m_sd->setWindowModality(Qt::WindowModal);
+
+            // start loading of TeaSafe image
+            m_loaderThread.setSharedIO(io);
+            m_loaderThread.start();
+            m_sd->exec();
+        }
+    }
 }
 
 void MainWindow::updateProgressSlot()
@@ -236,45 +250,63 @@ void MainWindow::doWork(WorkType workType)
 
             delete *it;
         } else if(workType == WorkType::ExtractItem) {
-            std::string fsPath = QFileDialog::getExistingDirectory().toStdString();
-            qDebug() << "ExtractItem";
-            f = boost::bind(teasafe::utility::extractToPhysical, boost::ref(*m_teaSafe),
-                            teaPath, fsPath);
+            QFileDialog dlg( NULL, tr("Filesystem folder to extract to.."));
+            dlg.setAcceptMode( QFileDialog::AcceptOpen );
+            dlg.setFileMode(QFileDialog::DirectoryOnly);
+            if(dlg.exec()) {
+                std::string fsPath = dlg.selectedFiles().at(0).toStdString();
 
+                qDebug() << "ExtractItem";
+                f = boost::bind(teasafe::utility::extractToPhysical, boost::ref(*m_teaSafe),
+                                teaPath, fsPath);
+            }
 
         } else if(workType == WorkType::CreateFolder) {
             qDebug() << "CreateFolder";
             if(m_teaSafe->folderExists(teaPath)) {
+                bool ok;
                 std::string folderName = QInputDialog::getText(this, tr("New folder name dialog"),
-                                                               tr("Folder name:"), QLineEdit::Normal).toStdString();
+                                                               tr("Folder name:"), QLineEdit::Normal,"",&ok).toStdString();
 
-                std::string path((*it)->text(0).toStdString() == "/" ?
-                                 teaPath.append(folderName) :
-                                 teaPath.append("/").append(folderName));
+                if((!folderName.empty()) && ok) {
+                    std::string path((*it)->text(0).toStdString() == "/" ?
+                                     teaPath.append(folderName) :
+                                     teaPath.append("/").append(folderName));
 
-                f = boost::bind(&teasafe::TeaSafe::addFolder, m_teaSafe, path);
-                QTreeWidgetItem *item = new QTreeWidgetItem(*it);
-                item->setChildIndicatorPolicy (QTreeWidgetItem::ShowIndicator);
-                item->setText(0, QString(folderName.c_str()));
+                    f = boost::bind(&teasafe::TeaSafe::addFolder, m_teaSafe, path);
+                    QTreeWidgetItem *item = new QTreeWidgetItem(*it);
+                    item->setChildIndicatorPolicy (QTreeWidgetItem::ShowIndicator);
+                    item->setText(0, QString(folderName.c_str()));
+                }
             }
         } else if(workType == WorkType::AddFolder) {
 
             if(m_teaSafe->folderExists(teaPath)) {
-                std::string fsPath = QFileDialog::getExistingDirectory().toStdString();
-                f = boost::bind(&teasafe::utility::copyFromPhysical, boost::ref(*m_teaSafe),
-                                teaPath, fsPath);
-                QTreeWidgetItem *item = new QTreeWidgetItem(*it);
-                item->setChildIndicatorPolicy (QTreeWidgetItem::ShowIndicator);
-                item->setText(0, QString(boost::filesystem::path(fsPath).filename().c_str()));
+                QFileDialog dlg( NULL, tr("Folder to add.."));
+                dlg.setAcceptMode( QFileDialog::AcceptOpen );
+                dlg.setFileMode(QFileDialog::DirectoryOnly);
+                if(dlg.exec()) {
+                    std::string fsPath = dlg.selectedFiles().at(0).toStdString();
+                    f = boost::bind(&teasafe::utility::copyFromPhysical, boost::ref(*m_teaSafe),
+                                    teaPath, fsPath);
+                    QTreeWidgetItem *item = new QTreeWidgetItem(*it);
+                    item->setChildIndicatorPolicy (QTreeWidgetItem::ShowIndicator);
+                    item->setText(0, QString(boost::filesystem::path(fsPath).filename().c_str()));
+                }
             }
         }  else if(workType == WorkType::AddFile) {
 
             if(m_teaSafe->folderExists(teaPath)) {
-                std::string fsPath = QFileDialog::getOpenFileName().toStdString();
-                f = boost::bind(&teasafe::utility::copyFromPhysical, boost::ref(*m_teaSafe),
-                                teaPath, fsPath);
-                QTreeWidgetItem *item = new QTreeWidgetItem(*it);
-                item->setText(0, QString(boost::filesystem::path(fsPath).filename().c_str()));
+                QFileDialog dlg( NULL, tr("File to add.."));
+                dlg.setAcceptMode( QFileDialog::AcceptOpen );
+                dlg.setFileMode(QFileDialog::AnyFile);
+                if(dlg.exec()) {
+                    std::string fsPath = dlg.selectedFiles().at(0).toStdString();
+                    f = boost::bind(&teasafe::utility::copyFromPhysical, boost::ref(*m_teaSafe),
+                                    teaPath, fsPath);
+                    QTreeWidgetItem *item = new QTreeWidgetItem(*it);
+                    item->setText(0, QString(boost::filesystem::path(fsPath).filename().c_str()));
+                }
             }
         }
         if(f) {
