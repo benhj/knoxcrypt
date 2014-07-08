ae97e700e24ce00dc56882ad34c6788fe896bc12
benhj <bhj.research@gmail.com>
2014-07-08 08:06:58 -0400

    Fixed xtea cipher comment

--- src/cipher/XTEAByteTransformer.cpp
+++ src/cipher/XTEAByteTransformer.cpp
@@ -63,15 +63,15 @@ namespace teasafe { namespace cipher
         void convertBytesAndEncipher(unsigned int num_rounds, unsigned char * buffer, uint32_t const key[4])
         {
 
-            // convert the input buffer to 2 64bit integers
+            // convert the input buffer to 2 32bit integers
             uint32_t datablock[2];
             datablock[0] = (buffer[0] << 24) | (buffer[1] << 16)  | (buffer[2] << 8) | (buffer[3]);
             datablock[1] = (buffer[4] << 24) | (buffer[5] << 16)  | (buffer[6] << 8) | (buffer[7]);
 
-            // encrypt the 128 bit number
+            // encrypt the 64 bit number
             encipher(num_rounds, datablock, key);
 
-            // convert the two 64 bit numbers, now encrypted, to an 8-byte buffer again
+            // convert the two 32 bit numbers, now encrypted, to an 8-byte buffer again
             buffer[0] = static_cast<unsigned char>((datablock[0] >> 24) & 0xFF);
             buffer[1] = static_cast<unsigned char>((datablock[0] >> 16) & 0xFF);
             buffer[2] = static_cast<unsigned char>((datablock[0] >> 8) & 0xFF);
